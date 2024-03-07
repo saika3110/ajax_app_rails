d106f8a143a0a8df4a0731832854a1741feb6a7c
saika3110 <143299649+saika3110@users.noreply.github.com>
2024-03-07 21:36:21 +0900

    ミスコミット

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,7 +1,7 @@
 function post() {
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
